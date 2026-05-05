6d103507d487872770b9871d66f94cd13f390604
Renato <ren.rib.silva@gmail.com>
2026-05-04 23:01:24 -0300

    arquivos modificados

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,15 @@ vim.api.nvim_set_keymap(
   ':w<CR>:split | term npx sucrase-node "%"<CR>',
   { noremap = true, silent = true }
 )
+
+-- Bloqueia :q no neotree
+_G.check_q_block = function()
+  if vim.bo.filetype == "snacks_picker_list" or vim.bo.filetype == "snacks_explorer" then
+    return ""
+  else
+    return "q"
+  end
+end
+
+-- Cria a abreviação que se reavalia TODA vez
+vim.cmd([[cnoreabbrev <expr> q v:lua.check_q_block()]])
