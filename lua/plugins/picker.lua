b4578c449d0ef3369ae606dee2d7e4cb7789b51d
Renato <ren.rib.silva@gmail.com>
2026-05-07 16:34:45 -0300

    arquivos modificados

---
+++ lua/plugins/picker.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      layouts = {
+        sidebar = {
+          layout = { width = 30, max_width = 30 },
+        },
+      },
+    },
+  },
+}
