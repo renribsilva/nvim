d4446a079f61e5cbb66372b6ab8843e8736badb3
Renato <ren.rib.silva@gmail.com>
2026-05-08 21:04:10 -0300

    arquivos modificados

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>to", ":split|term<CR>i", { desc = "Inicia um terminal" })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Sai do modo inserção" })
+vim.keymap.set("n", "<leader>qb", ":bd<CR>:q<CR>", { desc = "Quit buffer" })
