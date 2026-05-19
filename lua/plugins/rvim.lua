1559e038fbccc439ed36d6aed66e76f2e7e116b4
Renato <ren.rib.silva@gmail.com>
2026-05-18 21:06:19 -0300

    arquivos modificados

--- lua/plugins/rvim.lua
+++ lua/plugins/rvim.lua
@@ -14,12 +14,12 @@ return {
           end,
         },
         -- Não salva o ambiente de trabalho quando a sessão é encerrada
-        R_args = { "--no-save" },
+        R_args = { "--no-save", "--quiet" },
         -- Configura o mode de vizualização do console
-        R_app = "radian",
+        R_app = "R",
         min_editor_width = 10,
         rconsole_width = 50,
-        -- navega pelos nomes das variáveis e executa comandos R sem precisar digitar o nome do objeto
+        hl_term = false, -- navega pelos nomes das variáveis e executa comandos R sem precisar digitar o nome do objeto
         objbr_mappings = {
           c = "class",
           ["<localleader>gg"] = "head({object}, n = 15)",
